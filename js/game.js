e487db3dfebbb8e4a126d8f25e8b5bcc9e317aaf
Alex Ly <alexly@Alexs-MBP.home>
2015-11-11 20:59:44 -0500

    added results function

--- js/game.js
+++ js/game.js
@@ -24,6 +24,8 @@ function clickFriend(e){
 		alreadyClicked.push(clicked);
 		counter++;
 	}
+
+	console.log(alreadyClicked);
 }
 
 startButton.addEventListener('click', startGame);
@@ -32,13 +34,36 @@ startButton.addEventListener('click', startGame);
 
 function startGame(){
 	table.addEventListener('click', clickFriend);
-	startButton.removeEventListener('click', startGame);
+	startButton.className = "poof";
 
 	setTimeout(function(){
 		table.removeEventListener('click', clickFriend);
-		console.log('you done, fool!');
 		// call results function here
+		finalResult();
 	}, 20000)
 }
 
 // results function
+function finalResult(){
+	var finalCount = document.getElementById('final');
+	var msgArea = document.createElement('p');
+
+	finalCount.appendChild(msgArea);
+
+	if(counter === 0){
+		msgArea.textContent = "You clicked " + counter + " out of the 20 pictures.  You either passed out or you have no fingers.";
+	}else if(counter >0 && counter <= 5){
+		msgArea.textContent = msgArea.textContent = "You clicked " + counter + " out of the 20 pictures.  Drunk, you have to be drunk...";
+	}else if(counter >5 && counter <= 10){
+		msgArea.textContent = "You clicked " + counter + " out of the 20 pictures.  You're still on the bottom part of the clicking bell curve.";
+	}else if(counter >10 && counter <=15){
+		msgArea.textContent = "You clicked " + counter + " out of the 20 pictures. You're getting there.  Don't get cocky, kid.";
+	}else if(counter > 15 && counter <20){
+		msgArea.textContent = "You clicked " + counter + " out of the 20 pictures. You're well on your way to becoming a click-master.";
+	}else if(counter === 20){
+		msgArea.textContent = "You clicked " + counter + " out of the 20 pictures. You're a true click-master.  Your iron finger probably breaks mice in weeks.  They will write legends about you, or at least you'll be mentioned in the footnote of a blog somewhere.";
+	}
+
+	finalCount.className += "appear";
+	msgArea.className = "final-results";
+}
