2d9036e8c0f7afc32009dbc38c2b737d1934cdef
Mohammed Rizvi <morizvi07@gmail.com>
2016-01-12 10:14:00 -0500

    Removed previous final results functions to new one where modal pops up with the number of clicks the user hit and a funny gif

--- js/game.js
+++ js/game.js
@@ -1,78 +1,81 @@
-var table = document.getElementById('game-table');
-var startButton = document.getElementById('start-button');
-var counter = 0;
-var alreadyClicked = [];
+$(document).ready(function(){
 
+  var table = document.getElementById('game-table');
+  var startButton = document.getElementById('start-button');
+  var counter = 0;
+  var alreadyClicked = [];
 
 // click function
 
-function clickFriend(e){
+  function clickFriend(e){
 
-	var clicked = e.target.id;
-	var alreadyLength = alreadyClicked.length;
+    var clicked = e.target.id;
+    var alreadyLength = alreadyClicked.length;
+      if(alreadyLength == 0){
+        alreadyClicked.push(clicked);
+        counter++;
+      } else if(alreadyLength > 0){
+          for(var i=0; i<=alreadyLength; i++){
+            if(alreadyClicked[i] == clicked){
+              console.log('already clicked');
+              return false;
+            }
+          }
+          alreadyClicked.push(clicked);
+          counter++;
+        }
+        console.log(alreadyClicked);
+  }
 
-	if(alreadyLength == 0){
-		alreadyClicked.push(clicked);
-		counter++;
-	}else if(alreadyLength > 0){
-		for(var i=0; i<=alreadyLength; i++){
-			if(alreadyClicked[i] == clicked){
-				console.log('already cliked');
-				return false;
-			}
-		}
-		alreadyClicked.push(clicked);
-		counter++;
-	}
-
-	console.log(alreadyClicked);
-}
-
-startButton.addEventListener('click', startGame);
+  startButton.addEventListener('click', startGame);
 
 // start game and set time
 
-function startGame(){
-	table.addEventListener('click', clickFriend);
-	startButton.className = "poof";
+  function startGame(){
+    table.addEventListener('click', clickFriend);
 
-	setTimeout(function(){
-		table.removeEventListener('click', clickFriend);
-		// call results function here
-		finalResult();
-	}, 20000);
-}
+    setTimeout(function(){
+      table.removeEventListener('click', clickFriend);
+      // call results function here
+      finalResult();
+    }, 20000);
+  }
 
 // results function
-function finalResult(){
-	var finalCount = document.getElementById('final');
-	var msgArea = document.createElement('p');
+  function finalResult(){
+    if(counter === 0){
+      $("h4").html("You clicked " + counter + " out of the 20 pictures. You Suck!");
+        $("p").html("<img id='gif-width' src='http://i.imgur.com/1HWQIPa.gif'>");
+          $("#myModal").modal("show");  
+    } else if(counter >0 && counter <= 5){
+        $("h4").html("You clicked " + counter + " out of the 20 pictures. Are you even trying?");
+          $("p").html("<img id='gif-width' src='https://31.media.tumblr.com/f699c1259455f91745a4ece7742d447e/tumblr_nbr2j0O1bt1ruqqm0o1_500.gif'>");
+            $("#myModal").modal("show");
+    } else if(counter >5 && counter <= 10){
+        $("h4").html("You clicked " + counter + " out of the 20 pictures. You need to step it up!");
+          $("p").html("<img id='gif-width' src='https://45.media.tumblr.com/tumblr_lw2545XdCg1qdpgsdo1_500.gif'>");
+            $("#myModal").modal("show");
+    } else if(counter >10 && counter <=15){
+        $("h4").html("You clicked " + counter + " out of the 20 pictures. Okay Not bad");
+          $("p").html("<img id='gif-width' src='https://s-media-cache-ak0.pinimg.com/originals/e5/91/ed/e591ed7a8f94dfb1a2de49bc7048491b.gif'>");
+            $("#myModal").modal("show");
+    } else if(counter > 15 && counter <20){
+        $("h4").html("You clicked " + counter + " out of the 20 pictures. Nice Job Bro!");
+          $("p").html("<img id='gif-width' src='http://3.bp.blogspot.com/-iEse-1-jcQM/Vd4T3nfV46I/AAAAAAAAD0g/W19FoXKXy_A/s320/youre-awesome.gif'>");
+            $("#myModal").modal("show");
+    } else if(counter === 20){
+        $("h4").html("You clicked " + counter + " out of the 20 pictures. Wow Awesome Job Bro!");
+          $("p").html("<img id='gif-width' src='http://gifrific.com/wp-content/uploads/2012/08/LeBron-James-Dancing-After-Winning-Gold-Medal-2012-London-Olympics.gif'>");
+            $("#myModal").modal("show");  
+    } 
+  }
+});
+/*function shamoonsAlert(){
+  setTimeout(function(){
+    alert('Assignment called for an alert:  You clicked on ' + counter + " pictures.");
+  }, 4000);
+}*/
 
-	finalCount.appendChild(msgArea);
 
-	if(counter === 0){
-		msgArea.textContent = "You clicked " + counter + " out of the 20 pictures.  You either passed out or you have no fingers.";
-	}else if(counter >0 && counter <= 5){
-		msgArea.textContent = msgArea.textContent = "You clicked " + counter + " out of the 20 pictures.  Someone had a few, didn't they?";
-	}else if(counter >5 && counter <= 10){
-		msgArea.textContent = "You clicked " + counter + " out of the 20 pictures.  You know you're supposed to click on the pictures, right?";
-	}else if(counter >10 && counter <=15){
-		msgArea.textContent = "You clicked " + counter + " out of the 20 pictures. You're getting there.  Don't get cocky, kid.";
-	}else if(counter > 15 && counter <20){
-		msgArea.textContent = "You clicked " + counter + " out of the 20 pictures. You're well on your way to becoming a click-master.";
-	}else if(counter === 20){
-		msgArea.textContent = "You clicked " + counter + " out of the 20 pictures. You're a true click-master.  Your iron finger probably breaks mice in weeks.  They will write legends about you, or at least you might be mentioned in the footnote of a blog somewhere.";
-	}
 
-	table.className = "poof";
-	finalCount.className += "appear";
-	msgArea.className = "final-results";
 
-	shamoonsAlert();
-}
-
-function shamoonsAlert(){
-	setTimeout(function(){
-		alert('Assignment called for an alert:  You clicked on ' + counter + " pictures.");
-	}, 4000);
-}
